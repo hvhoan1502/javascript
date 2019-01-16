eb6ae632573a7de526673f3f1d10016791a81a55
hvhoan1502 <hvhoan1502@gmail.com>
2019-01-16 14:36:59 +0700

    b2

---
+++ baigiang/buoi-2.js
@@ -0,0 +1,52 @@
+// for (let i = 1; i< 10; i++) {
+//     console.log(i);
+// }
+
+// let a = 5;
+// while(a < 10) {
+//     console.log(a);
+// }
+
+
+// let b = 5;
+// do {
+//     console.log(b);
+//     b += 2;
+// } while (b < 8);
+
+
+// const chon = 11;
+
+// switch(chon) {
+//     case 1:
+//         console.log('Bạn lựa chọn: ' + chon);
+//         break;
+//     case 2:
+//         console.log('Bạn lựa chọn: ' + chon);
+//         break;
+//     case 3:
+//         console.log('Bạn lựa chọn: ' + chon);
+//         break;
+//     case 4:
+//         console.log('Bạn lựa chọn: ' + chon);
+//         break;
+//     case 5:
+//         console.log('Bạn lựa chọn: ' + chon);
+//         break;
+//     case 6:
+//         console.log('Bạn lựa chọn: ' + chon);
+//         break;
+//     case 7:
+//         console.log('Bạn lựa chọn: ' + chon);
+//         break;
+//     case 8:
+//         console.log('Bạn lựa chọn: ' + chon);
+//         break;
+//     case 10:
+//     case 11:
+//     case 12:
+//         console.log('Bạn đã vô lựa chọn đặc biệt ' + chon);
+//         break;
+//     default: 
+//         console.log('Vui lòng nhập lại');
+// }
---
+++ baitap/buoi-2/bai-1.js
@@ -0,0 +1,7 @@
+/*
+    Cho mảng số nguyên: const mang = [1, 8, 9, 5, 12, 14, 13,14];
+    Sử dùng do-while kết hợp switch-case để chia thành 2 mảng số nguyên chẵn và lẻ
+    Ví dụ kết quả sẽ in ra:
+    Mảng số chẵn là: 8,12,14,14
+    Mảng số lẻ là: 1,5,9,13 
+*/
