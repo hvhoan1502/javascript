dd92c7c40377237dd053eac83955e861593bb474
hvhoan1502 <hvhoan1502@gmail.com>
2019-01-16 16:11:04 +0700

    bai-tap1

--- baitap/buoi-2/bai-1.js
+++ baitap/buoi-2/bai-1.js
@@ -5,3 +5,43 @@
     Mảng số chẵn là: 8,12,14,14
     Mảng số lẻ là: 1,5,9,13 
 */
+// Cách làm khó mình (Tham khảo)
+const mang = [1, 8, 9, 5, 12, 14, 13,14]
+let mangchan = [];
+
+let mangle = [];
+
+let item = 0;
+do {
+    const giatrithuI = mang[item];
+    let laydu = giatrithuI % 2;
+    switch(laydu) {
+        case 0:
+            mangchan.push(giatrithuI);
+            break;
+        case 1:
+            mangle.push(giatrithuI);
+            break;
+        default:
+            break;
+    }
+    item ++;
+} while (item < mang.length);
+
+
+console.log('Mảng chẵn là: ' + mangchan);
+
+console.log('Mảng lẻ là: ' + mangle);
+
+
+// cách dễ và tốt hơn
+for (let i = 0; i < mang.length; i++) {
+    if (mang[i] % 2== 0) {
+        mangchan.push(mang[i]);
+    } else {
+        mangle.push(mang[i])
+    }
+}
+
+console.log('Mảng chẵn là: ' + mangchan);
+console.log('Mảng lẻ là: ' + mangle);
