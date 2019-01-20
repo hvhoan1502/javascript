8de1ad485d0e3139dcb21fa893039c77230993fa
hvhoan1502 <hvhoan1502@gmail.com>
2019-01-20 12:07:56 +0700

    chuoi

---
+++ baigiang/buoi-3.js
@@ -0,0 +1,62 @@
+// const a = -6/0;
+
+
+// console.log(a);
+
+// const a = undefined;
+/**
+ * undefined, null, identifi thì js hiểu là mang giá trị false
+ * Khi giá trị của 1 biến là 0 hoặc 1 thì trong biểu thức so sánh js sẽ hiểu
+ * 0 là false và 1 là true
+ */
+
+
+// if (a) {
+//     console.log('a có giá trị');
+// } else {
+//     console.log('a không có giá trị');
+// }
+
+// let input = false;
+
+// if (input == undefined) {
+//     console.log('if');
+// } else {
+//     console.log('else');
+// }
+
+// const b = [];
+// if (b) {
+//     console.log('if');
+// } else {
+//     console.log('else');
+// }
+
+// let n = null;
+// console.log(n + 5);
+
+/************NUMBER */
+
+// const a = '3';
+// console.log(a+1);
+// console.log(a + 'hello' + 3);
+// console.log(5 - a);
+// console.log(5 + a);
+// console.log(parseInt('123'));
+// console.log(parseFloat('123.00012'));
+console.log(Math.ceil(1.562));
+console.log(Math.floor(1.5124));
+console.log(Math.round(1.5214));
+console.log(Math.ceil(Math.random() * 100));
+
+
+
+/**STRING */
+// const b = 'hi';
+// console.log(b + ' hoan');
+// console.log(b + 2);
+// console.log(b.concat(' Hoàn'));
+
+
+
+
--- baitap/buoi-2/bai-2.js
+++ baitap/buoi-2/bai-2.js
@@ -7,3 +7,22 @@
  /**
   * Viết hàm tính kết quả của phương trình bậc hai
   */
+
+
+/**
+ * Cho a = 10, b = 20;
+ * In ra các số từ 10 đến 18
+ */
+
+ /**
+  * Tính tổng các số chẵn lẻ từ 1 đến 100
+  */
+
+/**
+ * Viết chương trình có hai số nguyên x, y, bất kỳ sau đó 
+ * tính: p=x*y, s=x+y, total=s2+p(s-x)*(p+y) và in kết quả.
+ */
+
+ /**
+  * Cho 1 mảng [1, 2, 5, 9, 12, 15, 3, 20], tìm số lớn nhất và nhỏ nhất trong mảng.
+  */
